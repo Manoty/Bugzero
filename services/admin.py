09d2fa85055533a3b12fbe13dc137e5e8e49b214
Kevin Manoti <kevinnoty21@gmail.com>
2026-05-04 12:06:17 +0300

    register in admin

--- services/admin.py
+++ services/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import ServiceCategory, Service, Testimonial
 
-# Register your models here.
+
+@admin.register(ServiceCategory)
+class ServiceCategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug', 'order']
+    prepopulated_fields = {'slug': ('name',)}
+
+
+@admin.register(Service)
+class ServiceAdmin(admin.ModelAdmin):
+    list_display = ['name', 'category', 'property_type', 'price_min', 'price_max', 'unit', 'is_active']
+    list_filter = ['property_type', 'category', 'is_active']
+    list_editable = ['is_active', 'order']
+    prepopulated_fields = {'slug': ('name',)}
+    search_fields = ['name', 'pest_type']
+
+
+@admin.register(Testimonial)
+class TestimonialAdmin(admin.ModelAdmin):
+    list_display = ['author_name', 'role', 'rating', 'is_featured']
+    list_editable = ['is_featured']
